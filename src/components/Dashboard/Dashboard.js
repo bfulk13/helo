cf4831441eb69f0251da213a2ee0416b3892195e
Barry Fulk <bfulk@Barrys-MacBook-Pro.local>
2019-03-01 16:02:07 -0700

    part2 step1

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
+import './Dashboard.css';
 
 class Dashboard extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      input: '',
+      checkbox: true,
+      posts: []
+    }
+    this.toggleBox = this.toggleBox.bind(this);
+  }
+
+  componentDidMount(){
+    this.displayPosts();
+  }
+
+  displayPosts = () => {
+    this.state.posts.map(post => post)
+  }
+
+  toggleBox() {
+    if(!this.state.checkbox){
+      this.setState({ checkbox: true })
+    } else {
+      this.setState({ checkbox: false })
+    }
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className="mainDash">
         <h1>Dashboard</h1>
+        <input />
+        <button>Search</button>
+        <button>Reset</button>
+        My Posts <input value={ this.state.checkbox } type='checkbox' onClick={ this.toggleBox } />
       </div>
     );
   }
